edded9f82de1ffbf970cf35a6660bcfd370b2149
Vicky Huynhcao <vicky89@gmail.com>
2019-11-14 19:57:02 +0000

    Added componentDidUpdate component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,6 +10,14 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    this.componentDidUpdate();
+  }
+
+  componentDidUpdate() {
+    fetch('/api/grades')
+      .then(response => response.json())
+      .then(grades => this.setState({ grades }))
+      .catch(error => console.error('Fetch Error: ', error));
 
   }
 
