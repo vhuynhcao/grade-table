5fff9b8741d3e3cf6ed822837569501db9963ed5
Vicky Huynhcao <vicky89@gmail.com>
2019-11-14 19:36:01 +0000

    Gave the class App initial state of grades with empty array in constructor

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
   render() {
     return null;
   }
