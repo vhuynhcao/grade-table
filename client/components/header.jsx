aab06703896fce6209b369815214402f0e8259b7
Vicky Huynhcao <vicky89@gmail.com>
2019-11-15 00:16:11 +0000

    Finished View Average Grade feature set

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Header(props) {
   return (
-    <div>
-      <h1 className="mb-4">{ props.text}</h1>
-      <h2 className="badge">{props.average}</h2>
+    <div className="d-flex justify-content-between alight-items-center">
+      <h1 className="sgtTitle">{ props.text}</h1>
+      <h2 className="avgGrade">Average Grade: <span className="badge badge-secondary">{props.average}</span></h2>
     </div>
   );
 }
